4c6bfabde047955a80a978df25b70fd520a87190
Lastor70 <50678100+Lastor70@users.noreply.github.com>
2024-09-27 17:03:38 +0000

    Added fetched orders from crm

--- process_payment.py
+++ process_payment.py
@@ -2,13 +2,13 @@ import pandas as pd
 from google_sheets import authenticate_google_sheets
 
 def fetch_and_process_payment_sheet(gc, spreadsheet_id, sheet_name):
-    """Отримання та обробка даних з листка 'Выплата (new)'."""
+    """отримання та обробка даних з Выплата (new)"""
     worksheet_payment = gc.open_by_key(spreadsheet_id).worksheet(sheet_name)
     data_payment = worksheet_payment.get_all_values()
     headers_1 = data_payment.pop(0)
     df_payment = pd.DataFrame(data_payment, columns=headers_1)
 
-    # Обробка df_appruv_range
+    # обробка df_appruv_range
     
     df_appruv_range = df_payment.iloc[:, 5:7]
     df_appruv_range.columns = df_appruv_range.iloc[1]
@@ -16,12 +16,12 @@ def fetch_and_process_payment_sheet(gc, spreadsheet_id, sheet_name):
     df_appruv_range['Диапазон апрува'] = df_appruv_range['Диапазон апрува'].str.replace('[^0-9<>-]', '', regex=True)
     df_appruv_range = df_appruv_range[:5]
     
-    # Обробка df_payment
+    # обробка df_payment
     df_pay = df_payment.iloc[0:, 1:4].dropna(how='all')
     df_pay.columns = df_pay.iloc[1]
     df_pay = df_pay[2:]
     
-    # Функції для обробки числових даних
+    # функції для обробки числових даних
     def extract_numbers(string):
         return float(''.join(filter(str.isdigit, string)))
     
--- streamlit_app.py
+++ streamlit_app.py
@@ -5,34 +5,26 @@ from caching import *
 from data_processing_main_req import *
 from facebook_api import *
 
-# Налаштування сторінки
 st.set_page_config(page_title="Рассчет баеров", page_icon="📈")
-
-# Заголовки та фільтри
 st.title("Рассчет баеров")
 st.header('Фильтр по датам')
 
-# Отримання секретів з Streamlit
 api_key = st.secrets["api_key"]
 google_sheets_creds = st.secrets["gcp_service_account"]
 
-# Вибір категорії покупців
 buyers_list = ['ss', 'il', 'dm', 'mb']
 b = st.selectbox("Виберите категорию заказа", buyers_list)
 
-# Фільтр по датам
 start_date = st.date_input('Начальная дата', value=datetime(2024, 9, 1))
 end_date = st.date_input('Конечная дата', value=datetime(2024, 9, 3))
 
-# Валідація дат
 if end_date < start_date:
-    st.error('Конечная дата не может быть раньше начальной даты.')
+    st.error('Конечная дата не может быть раньше начальной даты')
 
-# Конвертація дат у строки
 start_date_str = start_date.strftime('%Y-%m-%d')
 end_date_str = end_date.strftime('%Y-%m-%d')
 
-# Отримання даних токенів з Google Sheets
+# отримання токенів з гуглшит
 spreadsheet_id_tokens = '1Q8eFscYd9dsl6QTzLiRQqKXMg3HFuZgwjd9kg0fOMdQ'
 sheet_name_tokens = 'Лист1'
 
@@ -40,7 +32,7 @@ df_tokens = fetch_tokens_data(spreadsheet_id_tokens, sheet_name_tokens, dict(goo
 if st.button("Fetch Tokens Data"):
     st.dataframe(df_tokens)
 
-# Отримання даних з оферів Google Sheets
+# отримання даних справочніка гуглшит
 spreadsheet_id_offers = '15GvP6wElztDSQKqk5kxnB37dKxKi3nTyEsTbBF1vqW4'
 
 combined_df = fetch_offers_data(spreadsheet_id_offers, dict(google_sheets_creds))
@@ -55,19 +47,18 @@ if st.button("Fetch Payment Data"):
     st.dataframe(df_appruv_range)
 
 
-
+df_grouped = cached_fetch_facebook_data(df_tokens, start_date_str, end_date_str)
 if st.button("Fetch Facebook Data"):
-    df_grouped = cached_fetch_facebook_data(df_tokens, start_date_str, end_date_str)
     if df_grouped is not None:
         st.dataframe(df_grouped)
     else:
-        st.warning("Немає доступних токенів для отримання даних з Facebook.")
+        st.warning("Нету доступных токенов фб")
 
 
-
-# Отримання даних з CRM
+# отримання даних з CRM
 if st.button("Fetch Orders"):
     df_orders = fetch_orders_data(api_key, start_date_str, end_date_str, b)
-    print(df_orders)
-    processed_orders = process_orders_data(df_orders)
+    # print(df_orders)
+
+    processed_orders = process_orders_data(df_orders, combined_df, df_payment, df_appruv_range, df_grouped, b)
     st.write(processed_orders)
